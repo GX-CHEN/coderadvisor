9c5e56aab50797aba7b5adca61f609082956cce5
Gongxia Chen <chengongxia1990@gmail.com>
2020-03-17 04:41:40 -0400

    add listing view initial code

--- client/src/components/Listing.js
+++ client/src/components/Listing.js
@@ -1,10 +1,19 @@
 import React from "react";
+import { Row, Col, Card } from "antd";
+import "./Listing.css";
 
 class Listing extends React.Component {
   render() {
     return (
       <React.Fragment>
-        <div>Listing Page</div>
+        <h1 className="listing-header">Listing Page</h1>
+        <div className="listing-container">
+          <Row>
+            <Col xs={24} sm={12}><Card></Card></Col>
+            <Col xs={24} sm={12}><Card></Card></Col>
+            <Col xs={24} sm={12}><Card></Card></Col>
+          </Row>
+        </div>
       </React.Fragment>
     );
   }
